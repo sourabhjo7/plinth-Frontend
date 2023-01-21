f4b9465bd3e206811b2a22f4c6af17c3b51f91aa
Kingshuk Ghosh <92975866+weeeb-coder@users.noreply.github.com>
2023-01-21 15:14:06 +0530

    build folder

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0226989aeff1473c9bd9d7e8983093bc.js"
+  "/precache-manifest.9b39401921a44ce496878e8d9b5997a0.js"
 );
 
 workbox.clientsClaim();
